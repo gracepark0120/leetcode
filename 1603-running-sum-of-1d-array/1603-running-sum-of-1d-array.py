fccbb645eccfc5ec77e32e48f4714fbe3f30888c
gracepark0120 <67902024+gracepark0120@users.noreply.github.com>
2025-06-14 18:51:21 +0900

    Time: 0 ms (100%), Space: 18.1 MB (23.75%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -1,9 +1,12 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         output = [0] * len(nums)
-        for i in range(len(nums)):
-            output[i] = (sum(nums[:i+1]))
+        output[0] = nums[0]
+        for i in range(1, len(nums)):
+            output[i] = output[i - 1] + nums[i]
         return output
 
-
-# 위 방법은 O(n^2), 매번 합을 다시 계산함. 이것보다 누적합을 계산하는 게 더 효율적임. O(n)
+# 아래 방법은 O(n^2), 매번 합을 다시 계산함. 이것보다 누적합을 계산하는 게 더 효율적임. O(n)
+        # for i in range(len(nums)):
+        #     output[i] = (sum(nums[:i+1]))
+        # return output
