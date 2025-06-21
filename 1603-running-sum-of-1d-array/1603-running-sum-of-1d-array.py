311f69a639db5c06ec46a0206a79fa4e2092c2aa
Grace <67902024+gracepark0120@users.noreply.github.com>
2025-06-22 01:53:41 +0900

    Time: 0 ms (100%), Space: 18.1 MB (23.45%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -1,12 +1,17 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        output = [0] * len(nums)
-        output[0] = nums[0]
-        for i in range(1, len(nums)):
-            output[i] = output[i - 1] + nums[i]
-        return output
+        for i in range(1, len(nums)): # 기존 nums 배열 사용, in-place 처리 공간 O(1)
+            nums[i] += nums[i - 1]
+        return nums
 
-# 아래 방법은 O(n^2), 매번 합을 다시 계산함. 이것보다 누적합을 계산하는 게 더 효율적임. O(n)
-        # for i in range(len(nums)):
-        #     output[i] = (sum(nums[:i+1]))
-        # return output
+
+    # 기존 작성 코드. 문제는 1. 새 배열 만든것 2. 루프 문이 i+1 시작이라 불필요한 계산 시간 추가 
+    # def runningSum(self, nums: List[int]) -> List[int]:
+    #     # nums[0] nums[0]+nums[1] nums[0]+..+nums[2] 
+    #     # ans[0] ans[1] ans[2]
+    #     result = [0]*len(nums) # 새 배열 만들기 공간 O(n)
+    #     result[0] = nums[0]
+    #     for i in range(len(nums)-1):
+    #         result[i+1] = result[i] + nums[i+1] # 이렇게 하면 인덱스 계산 시간이 미세하게 추가됨
+    #         #print(result[i+1])
+    #     return result
