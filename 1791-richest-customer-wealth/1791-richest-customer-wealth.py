c714f0b6a7e2e05492740d36dcd9eba31f9cb0b6
Grace <67902024+gracepark0120@users.noreply.github.com>
2025-06-22 02:30:16 +0900

    Time: 0 ms (100%), Space: 18.1 MB (13.62%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.py
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.py
@@ -1,6 +1,11 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         return max(map(sum, accounts))
+
+
+        # map 사용해서 메모리 아끼는 게 관건.
+        # map(function, iterable)
+        
         # wealth = [0]*len(accounts)
         # for i in range(len(accounts)):
         #     wealth[i] = sum(accounts[i])
